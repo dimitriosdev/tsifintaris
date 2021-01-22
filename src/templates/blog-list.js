feba2cb6c1ce0c6609df743f0457824dead2bfa1
dimitrios <33548247+dimitriosdev@users.noreply.github.com>
2021-01-22 14:23:03 +0100

    Update blog-list.js

--- src/templates/blog-list.js
+++ src/templates/blog-list.js
@@ -42,7 +42,7 @@ const Pagination = (props) => (
       {!props.isFirst && (
         <li>
           <Link to={props.prevPage} rel="prev">
-          <span className="icon -left"><RiArrowLeftLine/></span> Previous
+          <span className="icon -left"><RiArrowLeftLine/></span> Προηγούμενο
           </Link>
         </li>
       )}
@@ -59,7 +59,7 @@ const Pagination = (props) => (
       {!props.isLast && (
         <li>
           <Link to={props.nextPage} rel="next">
-            Next <span className="icon -right"><RiArrowRightLine/></span>
+            Επόμενο <span className="icon -right"><RiArrowRightLine/></span>
           </Link>
         </li>
       )}
@@ -98,7 +98,7 @@ class BlogIndex extends React.Component {
           title={"Blog — Page " + currentPage + " of " + numPages}
           description={"Stackrole base blog page " + currentPage + " of " + numPages }
         />
-        <h1>Blog</h1>
+        <h1>Άρθρα</h1>
         <div className="grids col-1 sm-2 lg-3">
           {posts}
         </div>
